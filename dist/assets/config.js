be7edcbb39d4e30e394f4283057658d158c06ea8
jyh <admin@hbcraft.cn>
2021-11-09 12:22:18 +0800

    chore: add dist to git

---
+++ dist/assets/config.js
@@ -0,0 +1,15 @@
+var conf = {
+  name: 'index',
+  address: 'http://file.hbcraft.cn/api',
+
+  visibilityOptions: {
+      size: {
+          use: true,
+          type: 'readable' //raw, readable, both
+      },
+      date: {
+          use: true,
+          type: 'moment' //raw, moment, both
+      }
+  }
+};
