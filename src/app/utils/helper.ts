b47de8bb768f597b22b33c2784637dcb392cefc9
hbcraft <admin@hbcraft.cn>
2022-07-05 10:15:14 +0800

    修复文件大小单位大小写错误的问题

--- src/app/utils/helper.ts
+++ src/app/utils/helper.ts
@@ -4,7 +4,7 @@ export class Helper {
   static getReadableSize(size: Required<PropertyValue>['size']): string {
     const i = Math.floor(Math.log(size) / Math.log(1024))
     return (
-      +(size / Math.pow(1024, i)).toFixed(2) + ['B', 'kB', 'MB', 'GB', 'TB'][i]
+      +(size / Math.pow(1024, i)).toFixed(2) + ['B', 'KB', 'MB', 'GB', 'TB'][i]
     )
   }
   static makeSizeString(
